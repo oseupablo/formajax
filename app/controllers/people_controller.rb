11de7f1da0c5a8b84fcf381beb2b0f6641cbaae9
Pablo Silva <phsilbr@gmail.com>
2016-08-17 08:48:22 -0300

    Model, controller and view for Person

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,19 @@
+class PeopleController < ApplicationController
+
+  def index
+    @person = Person.new
+    @people = Person.all
+  end
+
+  def create
+    @person = Person.new(person_params)
+    @person.save
+  end
+
+  private
+
+  # strong parameters
+  def person_params
+    params.require(:person).permit(:name, :email)
+  end
+end
